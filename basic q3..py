3071ef30a31e7200f68448c7b1cc91bfa89585bd
shubhamnag14 <43213463+shubhamnag14@users.noreply.github.com>
2018-10-30 20:54:43 +0530

    Add files via upload

---
+++ basic q3..py
@@ -0,0 +1,5 @@
+#create a program that asks the user to enter their name and their age. print out a message addressed to them that tells them the yur that they wi8ll turn 100 yur old....
+name = input("what yur name:")
+age = int (input("enter yur age:"))
+year=str((2018-age)+100)
+print( name +"will be 100 this yur"+year)
---
+++ basic q4..py
@@ -0,0 +1,6 @@
+x=str(input("ENTER THE NUMBER:"))
+y = x % 2
+if y>0:
+    print("EVEN")
+else:
+     print("ODD")
