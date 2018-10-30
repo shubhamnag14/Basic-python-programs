f61e92dcc9400bece742383b9bb3835795b7f96f
shubhamnag14 <43213463+shubhamnag14@users.noreply.github.com>
2018-10-30 22:00:47 +0530

    Add files via upload

---
+++ basic q1.py
@@ -0,0 +1,9 @@
+#additon of two numbers 
+num1 = int(input("enter the value:"))
+num2 = int(input("enter the value:"))
+s=int(num1 + num2)
+print(s)
+
+
+
+
---
+++ basic q2.py
@@ -0,0 +1,6 @@
+#concatenation of two strings
+str1 = 'Hello'
+str2 ='World!'
+
+print('str1 + str2 = ', str1 + str2)
+print('str1 * 2 =', str1 * 3)
